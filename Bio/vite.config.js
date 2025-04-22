573b0acb088d7d1a181a4d6ac25fa65710677225
pkzstar <114553213+pkzstar@users.noreply.github.com>
2025-04-22 17:07:10 -0500

    Update vite.config.js

--- Bio/vite.config.js
+++ Bio/vite.config.js
@@ -1,8 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Bio/',
+  base: '/Bio/',  // Make sure this matches the GitHub Pages URL path
   plugins: [react()],
 })
